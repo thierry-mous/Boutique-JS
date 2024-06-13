fe2c7c078c56353ce16b9c2374d77b58ca9874cf
ademlsgo <145021201+ademlsgo@users.noreply.github.com>
2024-06-13 10:58:56 +0200

    pull

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -5,12 +5,3 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(data);
         });
 });
-
-var counter = 1;
-setInterval(function() {
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter > 2) {
-        counter = 1;
-    }
-}, 3000);
